5b49cb6f0a2ddc398adec10684a433b48af9acfd
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2026-01-16 15:55:47 +0500

    no message displaying at server side at this time

--- 12-Networking_with_core_node_js_modules/01-Creating-UDP-Server-With-Node-JS/client.js
+++ 12-Networking_with_core_node_js_modules/01-Creating-UDP-Server-With-Node-JS/client.js
@@ -2,4 +2,4 @@ import dgram from 'node:dgram';
 
 const socket = dgram.createSocket('udp4');
 
-socket.send('Hi from Uzair',4000, '192.168.100.130')
+socket.send('Hi from Uzair Raza',4000, '192.168.100.130')//when we are sending message from client to server there is no msg displaying at client side because client is only sending message to server
