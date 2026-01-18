3b9bd3b43128cf1b9b2f01dab865b5fcd3c82757
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2026-01-18 21:57:07 +0500

    No error on client side even though send message on wrong port

--- 12-Networking_with_core_node_js_modules/01-Creating-UDP-Server-With-Node-JS/client.js
+++ 12-Networking_with_core_node_js_modules/01-Creating-UDP-Server-With-Node-JS/client.js
@@ -6,5 +6,5 @@ socket.on('message', (msg, remoteAddress) => {
     console.log(`${msg.toString()}`)
 });
 
-socket.send('Hi from Uzair Raza', 4000, '192.168.100.130')//when we are sending message from client to server there is no msg displaying at client side because client is only sending message to server
+socket.send('Hi from Uzair Raza', 1000, '192.168.100.130')//when we are sending message from client to server there is no msg displaying at client side because client is only sending message to server
 
