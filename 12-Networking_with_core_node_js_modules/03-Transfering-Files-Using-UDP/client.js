066992d8808c9fd389145733e2d9fd17dce633e2
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2026-01-20 16:09:28 +0500

    add progress bar on client terminal

--- 12-Networking_with_core_node_js_modules/03-Transfering-Files-Using-UDP/client.js
+++ 12-Networking_with_core_node_js_modules/03-Transfering-Files-Using-UDP/client.js
@@ -1,22 +1,31 @@
 import dgram from 'node:dgram'
-import { createReadStream } from 'node:fs'
-
+import { createReadStream, statSync } from 'node:fs'
+let lastPrintedPercent = 0
 const clientSocket = dgram.createSocket('udp4')
+const fileSize = statSync('C:\\Users\\PMLS\\Desktop\\numbers.txt').size
 const content = createReadStream(
   'C:\\Users\\PMLS\\Desktop\\numbers.txt',
-  { highWaterMark: 1000 } // stay under UDP payload limit
+  { highWaterMark: 1000 }, // stay under UDP payload limit
 )
 
+clientSocket.send(fileSize + '', 4000, '192.168.100.130')
 clientSocket.on('message', (msg, rinfo) => {
   console.log(msg.toString())
 })
-
 content.on('data', (chunk) => {
-  content.pause()
+    const percent = Math.floor((content.bytesRead / fileSize) * 100)
+
+    if (percent > lastPrintedPercent) {  // only print on next 1% increment
+      lastPrintedPercent = percent
+      console.log(`File Read: ${percent}%`)
+    }
+
   clientSocket.send(chunk, 4000, '192.168.100.130', () => {
     content.resume()
-  })
-})
+    
+    })
+  }
+)
 
 content.on('end', () => {
   clientSocket.send('EOF', 4000, '192.168.100.130', () => {
--- 12-Networking_with_core_node_js_modules/03-Transfering-Files-Using-UDP/server.js
+++ 12-Networking_with_core_node_js_modules/03-Transfering-Files-Using-UDP/server.js
@@ -3,26 +3,35 @@ import { createWriteStream } from 'node:fs'
 
 const serverSocket = dgram.createSocket('udp4')
 const writeStream = createWriteStream('numbers_received.txt')
-
+let isFileSize = true
+let fileSize = ''
 serverSocket.on('message', (msg, remoteAddress) => {
   // console.log(msg.toString())
-
-  const isEmptyIB = writeStream.write(msg)
-  if (!isEmptyIB) {
-    serverSocket.pause?.()
-    writeStream.once('drain', () => {
-      serverSocket.resume?.()
-    })
-  }
-
-  if (msg.toString() === 'EOF') {
-    writeStream.end()
-    serverSocket.send(
-      'content received successfully on server',
-      remoteAddress.port,
-      remoteAddress.address,
-    )
-    serverSocket.close()
+  if (isFileSize) {
+    fileSize = msg.toString()
+    console.log(fileSize)
+    isFileSize = false
+    return
+  } else {
+    if (msg.toString() === 'EOF') {
+      writeStream.end()
+      serverSocket.send(
+        'content received successfully on server',
+        remoteAddress.port,
+        remoteAddress.address,
+      )
+      serverSocket.close()
+    }
+    else {
+      const isEmptyIB = writeStream.write(msg)
+      if (!isEmptyIB) {
+        serverSocket.pause?.()
+        writeStream.once('drain', () => {
+          serverSocket.resume?.()
+        })
+      }
+    
+    }
   }
 })
 
